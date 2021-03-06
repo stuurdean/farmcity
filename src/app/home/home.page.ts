76de7f1d04cd42d7d2768558c604944638d9fa96
mashudu <mselepe747@gmail.com>
2021-03-06 19:16:49 +0200

    search
    search

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,7 +4,14 @@ import { ProductsService } from './../services/products.service';
 
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+<<<<<<< Updated upstream
 import { BehaviorSubject } from 'rxjs';
+=======
+import {first} from 'rxjs/operators'
+import { AngularFirestore } from '@angular/fire/firestore';
+import * as firebase from "firebase";
+import { snapshotChanges } from '@angular/fire/database';
+>>>>>>> Stashed changes
 
 
 @Component({
@@ -14,6 +21,10 @@ import { BehaviorSubject } from 'rxjs';
 
 })
 export class HomePage {
+sampleArr=[];
+resultArr=[];
+ public foodlist:any;
+ searchedUsers=[];
   slideOpts = {
     initialSlide: 0,
     spaceBetween: 0,
@@ -25,16 +36,26 @@ export class HomePage {
   promotion : any;
   promotionNo :any;
   vegetables : any;
+<<<<<<< Updated upstream
   cartCount : BehaviorSubject<number>;
   constructor( private firestoreservice :ProductsService,private cartservice : CartService) {}
+=======
+  constructor( private firestoreservice :ProductsService,private firestore:AngularFirestore) {}
+>>>>>>> Stashed changes
 
 
   private profile :Profile;
   ngOnInit() {
 
+<<<<<<< Updated upstream
 
     console.log(this.profile)
     this.firestoreservice.getProducts().snapshotChanges().subscribe(result=>{
+=======
+   
+
+   this.firestoreservice.getProducts().snapshotChanges().subscribe(result=>{
+>>>>>>> Stashed changes
 
 
       this.products=result;
@@ -47,8 +68,12 @@ export class HomePage {
 
     //getting fruits from firebase
 
+<<<<<<< Updated upstream
 
     this.firestoreservice.getFruits().snapshotChanges().subscribe(result=>{
+=======
+     this.firestoreservice.getFruits().snapshotChanges().subscribe(result=>{
+>>>>>>> Stashed changes
 
 
 
@@ -78,9 +103,97 @@ export class HomePage {
 
   }
 
+  
+
   view()
   {
     console.log("Clicked")
   }
+
+
+ // async filterList(evt){
+
+    //this.firestoreservice.getProducts().snapshotChanges().subscribe(result=>{
+
+     // console.log(result);
+     // this.foodlist= this.products=result;
+   // });
+   // const searchTeam=evt.srcElement.value;
+   // if(!searchTeam){
+   // return;
+
+   // }
+   // this.foodlist=this.foodlist.filter(currentfood=>{
+ // if(currentfood.name&& searchTeam){
+    
+  //  return(currentfood.name.toLowercase().indexOf(searchTeam.toLowercase())>-1 || currentfood)
+
+
+
+ // }
+
+
+  //  }
+
+  //  )
+
+ // }
+
+ search(event){
+let searchKey: string =event.target.value;
+let firstLetter= searchKey.toUpperCase();
+if(searchKey.length==0){
+this.sampleArr=[];
+this.resultArr=[];
+}
+
+if(this.sampleArr.length==0)
+{
+this.firestore.collection('products',ref=>ref.where('Search','==',firstLetter)).snapshotChanges()
+.subscribe(data=>{
+data.forEach(childData=>{
+
+  this.sampleArr.push(childData.payload.doc.data())
+}
+
+
+)
+
+
+
+}
+
+
+
+)
+
+}
+else{
+
+this.resultArr=[];
+this.sampleArr.forEach(val=>{
+
+let name:string=val['productName'];
+if(name.toUpperCase().startsWith(searchKey.toUpperCase()))
+{if(true){
+this.resultArr.push(val);
+}
+
+}
+
+}
+
+
+
+)
+
+}
+
+
+ }
+
+
+
+
 
 }
